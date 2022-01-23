b57379bfbc7d6a398a3be101ca26e3dd0133974a
Edfrancys <edfrancys.victor@outlook.com>
2022-01-23 10:59:17 -0300

    add fibonaci algorithm

---
+++ algoritimos/scripts/fibonaci.js
@@ -0,0 +1,28 @@
+const fibonanciEl = document.getElementById("sequenciaFibonaci");
+
+function sequenciaFibonaci (value) {
+
+    const loop = value - 1;    
+    let arr = []
+
+    for (let index = 0; index <= loop; index++) {      
+
+        let ultimoIndice = arr.length - 1;
+        
+        if (arr.length == 1) {            
+            valor = arr[ultimoIndice] + 1;            
+        } else if (arr.length > 1) {
+           
+            let penultimoIndice = ultimoIndice - 1;
+            valor = arr[ultimoIndice] + arr[penultimoIndice];            
+        }
+        else {
+            valor = 0;
+        }
+        
+        arr[index] = valor;               
+    }
+    
+    fibonanciEl.innerHTML = '[' + arr + ']';    
+    
+}
